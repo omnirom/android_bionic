543dfa0ceb8425acc472e02d3143a63c2481f2ad
Stefan Berger <s.berger81@gmail.com>
2015-03-11 22:58:02 +0100

    bionic: Get rid of "has text relocations" warn messages
    Change-Id: Idad317bebf01068568dbce63b8ba2500a0fa3397

--- linker/linker.cpp
+++ linker/linker.cpp
@@ -2223,8 +2223,6 @@ bool soinfo::LinkImage(const android_dlextinfo* extinfo) {
   if (has_text_relocations) {
     // Make segments writable to allow text relocations to work properly. We will later call
     // phdr_table_protect_segments() after all of them are applied and all constructors are run.
-    DL_WARN("%s has text relocations. This is wasting memory and prevents "
-            "security hardening. Please fix.", name);
     if (phdr_table_unprotect_segments(phdr, phnum, load_bias) < 0) {
       DL_ERR("can't unprotect loadable segments for \"%s\": %s",
              name, strerror(errno));
