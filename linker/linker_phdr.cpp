792df63bae3a71c529a977bab0189473aa837480
Humberto Borba <humberos@gmail.com>
2015-12-10 12:53:05 -0200

    bionic: Remove prelink support
    It was made for legacy devices at 4.4 like (Aries and P1).
    So, these devices are not present at >=omni-5.0.
    
    Actually this "hack" is broken.
    
    So this change does a source cleanup
    also it reduces deltas between AOSP.
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I7de8c18430a14a36be1bef2b672219b5ab78d623

--- linker/linker_phdr.cpp
+++ linker/linker_phdr.cpp
@@ -137,9 +137,6 @@ ElfReader::ElfReader(const char* name, int fd, off64_t file_offset, off64_t file
     : name_(name), fd_(fd), file_offset_(file_offset), file_size_(file_size),
       phdr_num_(0), phdr_mmap_(nullptr), phdr_table_(nullptr), phdr_size_(0),
       load_start_(nullptr), load_size_(0), load_bias_(0),
-#ifdef ENABLE_PRELINK_SUPPORT
-      required_base_(0),
-#endif
       loaded_phdr_(nullptr) {
 }
 
@@ -304,38 +301,6 @@ size_t phdr_table_get_load_size(const ElfW(Phdr)* phdr_table, size_t phdr_count,
   return max_vaddr - min_vaddr;
 }
 
-#ifdef ENABLE_PRELINK_SUPPORT
-typedef struct {
-    long mmap_addr;
-    char tag[4]; /* 'P', 'R', 'E', ' ' */
-} prelink_info_t;
-
-/* Returns the requested base address if the library is prelinked,
- * and 0 otherwise.  */
-static ElfW(Addr) is_prelinked(int fd, const char *name)
-{
-    off_t sz = lseek(fd, -sizeof(prelink_info_t), SEEK_END);
-    if (sz < 0) {
-        DL_ERR("lseek() failed!");
-        return 0;
-    }
-
-    prelink_info_t info;
-    int rc = TEMP_FAILURE_RETRY(read(fd, &info, sizeof(info)));
-    if (rc != sizeof(info)) {
-        DL_ERR("Could not read prelink_info_t structure for `%s`\n", name);
-        return 0;
-    }
-
-    if (memcmp(info.tag, "PRE ", 4)) {
-        DL_ERR("`%s` is not a prelinked library\n", name);
-        return 0;
-    }
-
-    return (unsigned long)info.mmap_addr;
-}
-#endif
-
 // Reserve a virtual address range big enough to hold all loadable
 // segments of a program header table. This is done by creating a
 // private anonymous mmap() with PROT_NONE.
@@ -346,9 +311,7 @@ bool ElfReader::ReserveAddressSpace(const android_dlextinfo* extinfo) {
     DL_ERR("\"%s\" has no loadable segments", name_);
     return false;
   }
-#ifdef ENABLE_PRELINK_SUPPORT
-  required_base_ = is_prelinked(fd_, name_);
-#endif
+
   uint8_t* addr = reinterpret_cast<uint8_t*>(min_vaddr);
   void* start;
   size_t reserved_size = 0;
@@ -376,13 +339,7 @@ bool ElfReader::ReserveAddressSpace(const android_dlextinfo* extinfo) {
       return false;
     }
     int mmap_flags = MAP_PRIVATE | MAP_ANONYMOUS;
-#ifndef ENABLE_PRELINK_SUPPORT
     start = mmap(mmap_hint, load_size_, PROT_NONE, mmap_flags, -1, 0);
-#else
-    if (required_base_ != 0)
-      mmap_flags |= MAP_FIXED;
-    start = mmap((void*)required_base_, load_size_, PROT_NONE, mmap_flags, -1, 0);
-#endif
     if (start == MAP_FAILED) {
       DL_ERR("couldn't reserve %zd bytes of address space for \"%s\"", load_size_, name_);
       return false;
