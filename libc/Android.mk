f1442fd834d7d24f5815255cf48a8710681bc900
Ganadist <ganadist@gmail.com>
2016-09-04 11:37:03 +0800

    mako: don't use clang
    don't use clang to avoid breaking sensors
    based on
    https://github.com/ganadist/device_lge_occam/commit/09409abdcdbe737f07402ff4b77109e9682a2805
    
    Change-Id: I56e1992f4a0d88b8b7e83aa6620196edb03e1d2d

--- libc/Android.mk
+++ libc/Android.mk
@@ -689,7 +689,11 @@ LOCAL_CONLYFLAGS := $(libc_common_conlyflags)
 LOCAL_CPPFLAGS := $(libc_common_cppflags)
 LOCAL_C_INCLUDES := $(libc_common_c_includes)
 LOCAL_MODULE := libc_stack_protector
+ifeq ($(PRODUCT_DEVICE),mako)
+LOCAL_CLANG := false
+else
 LOCAL_CLANG := $(use_clang)
+endif
 LOCAL_ADDITIONAL_DEPENDENCIES := $(libc_common_additional_dependencies)
 LOCAL_CXX_STL := none
 LOCAL_SYSTEM_SHARED_LIBRARIES :=
